a3fec8cc12e634e785d54dbbd18ffca8fa7a51a8
Rodrigo_2@DEKTOPm2ADRJ2M <g.rodrigorodrigo@gmail.com>
2025-02-19 21:19:46 -0500

    agrego login descomentado

--- FrontEnd/src/Routes/Login.jsx
+++ FrontEnd/src/Routes/Login.jsx
@@ -1,20 +1,5 @@
-<<<<<<< HEAD
 import LoginFormulario from "../Components/Login/LoginFormulario";
 
 export const Login = () => {
   return <LoginFormulario />;
-=======
-import { LoginFormulario } from "../Components/Header/LoginFormulario";
-
-export const Login = () => {
-  return (
-    
-      <LoginFormulario/>
-   
-  );
->>>>>>> backend-vilma
 };
-
-
-
-
