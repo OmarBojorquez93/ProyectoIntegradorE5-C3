28db84010a4a5406458efa6255cba90751547aa4
Rodrigo_2@DEKTOPm2ADRJ2M <g.rodrigorodrigo@gmail.com>
2025-02-21 16:27:03 -0500

    Agrego formulario Crear Cuenta/iniciar Sesión

--- FrontEnd/src/Routes/Login.jsx
+++ FrontEnd/src/Routes/Login.jsx
@@ -1,5 +1,16 @@
+<<<<<<< HEAD
 import LoginFormulario from "../Components/Login/LoginFormulario";
 
 export const Login = () => {
   return <LoginFormulario />;
+=======
+import { LoginFormulario } from "../Components/Header/LoginFormulario";
+
+export const Login = () => {
+  return (
+    
+      <LoginFormulario/>
+   
+  );
+>>>>>>> 5038119 (Agrego formulario Crear Cuenta/iniciar Sesión)
 };
