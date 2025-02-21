c7c7480247ca0d135a4dcf8cb8ea35e3302e211f
Rodrigo_2@DEKTOPm2ADRJ2M <g.rodrigorodrigo@gmail.com>
2025-02-21 17:03:36 -0500

    Agrego icono estrella para la puntuacion , moneda y efecto en card

--- FrontEnd/src/Components/utils/Card.jsx
+++ FrontEnd/src/Components/utils/Card.jsx
@@ -4,24 +4,9 @@ import "./Card.css";
 
 export const Card = (product) => {
   const hasCategory = !!product.products?.category;
+  const hasProduct = product.product?.disponible;
 
-<<<<<<< HEAD
-=======
-  // Función para renderizar las estrellas según la puntuación
-  const renderStars = (rating) => {
-    const stars = [];
-    const totalStars = 5; // Máximo de estrellas
-    for (let i = 1; i <= totalStars; i++) {
-      if (i <= rating) {
-        stars.push(<FaStar key={i} className="star-icon filled" />); // Estrella llena
-      } else {
-        stars.push(<FaRegStar key={i} className="star-icon" />); // Estrella vacía
-      }
-    }
-    return stars;
-  };
-
->>>>>>> backend-vilma
+  console.log(hasProduct);
   return (
     <Link
       to={
@@ -51,25 +36,7 @@ export const Card = (product) => {
           {product.product?.descripcion && (
             <p className="card-description">{product.product?.descripcion}</p>
           )}
-<<<<<<< HEAD
-          {product.product?.disponible ? (
-            <p style={{ color: "#03adad" }}>Disponible</p>
-          ) : (
-            <p style={{ color: "red" }}>Agotado</p>
-=======
-          <div className="card-price-rating">
-            <span className="card-price">${product.product?.precio?.toFixed(2)}</span>
-            <div className="card-rating">
-              {renderStars(product.product?.rating || 0)} {/* Renderiza las estrellas */}
-              <span>({product.product?.rating || 0})</span> {/* Muestra el número de rating */}
-            </div>
-          </div>
-          {product.product?.disponible ? (
-            <p className="card-availability available">Disponible</p>
-          ) : (
-            <p className="card-availability sold-out">Agotado</p>
->>>>>>> backend-vilma
-          )}
+          {product.product?.disponible ? <p>Disponible</p> : <p>Agotado</p>}
         </div>
       )}
     </Link>
