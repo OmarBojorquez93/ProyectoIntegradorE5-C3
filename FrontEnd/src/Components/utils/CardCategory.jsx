477c506e7547fe9340c2fac23f4d88e08e48cfd0
Matias Torres Contreras <tcontreras993@gmail.com>
2025-02-25 10:27:53 -0600

    Cambios estilo en category

--- FrontEnd/src/Components/utils/CardCategory.jsx
+++ FrontEnd/src/Components/utils/CardCategory.jsx
@@ -1,20 +1,18 @@
 import { Link } from "react-router-dom";
 import "./CardCategory.css";
 const CardCategory = (product) => {
-  console.log(product);
   return (
-    <Link to={`category/${product.product?.category}`} className={``}>
-      <div className="">
-        <h2>{product.product?.category}</h2>
-      </div>
-
+    <Link
+      to={`category/${product.product?.category}`}
+      className={`card-category`}
+    >
       <img
         src={` /img${product.product?.img}`}
         alt={product.category}
         width={50}
         height={50}
-        className={``}
       />
+      <h2>{product.product?.category}</h2>
     </Link>
   );
 };
