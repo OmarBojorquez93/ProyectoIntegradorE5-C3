c02a5a10b5e91ca6aad47e56039ff112c60206ff
OmarBojorquez93 <bahena.bojorquez.omar@gmail.com>
2025-03-26 01:07:52 -0600

    Se agregan anotaciones de configuración de tests

--- Backend/src/test/java/com/impulse/back_end/BackEndApplicationTests.java
+++ Backend/src/test/java/com/impulse/back_end/BackEndApplicationTests.java
@@ -2,7 +2,9 @@ package com.impulse.back_end;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")  // Usa la configuración de tests
 @SpringBootTest
 class BackEndApplicationTests {
 
