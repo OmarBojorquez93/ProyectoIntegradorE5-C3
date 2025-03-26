94ae9a02573c27da6d3be2c1b432d7e618b27027
OmarBojorquez93 <bahena.bojorquez.omar@gmail.com>
2025-03-26 01:21:06 -0600

    Se modifica el parámetro ddl-auto y se quita la anotación anterior

--- Backend/src/test/java/com/impulse/back_end/BackEndApplicationTests.java
+++ Backend/src/test/java/com/impulse/back_end/BackEndApplicationTests.java
@@ -2,9 +2,7 @@ package com.impulse.back_end;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")  // Usa la configuración de tests
 @SpringBootTest
 class BackEndApplicationTests {
 
