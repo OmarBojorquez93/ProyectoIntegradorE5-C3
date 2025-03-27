89f2c5be899d11486c58dcd5a9a3f0b12fb448a9
OmarBojorquez93 <bahena.bojorquez.omar@gmail.com>
2025-03-27 16:13:47 -0600

    Se actualiza el enum de los roles de usuario para que asocie el 0 a usuario normal y 1 como admin

--- Backend/src/main/java/com/impulse/back_end/Entity/UsuarioRole.java
+++ Backend/src/main/java/com/impulse/back_end/Entity/UsuarioRole.java
@@ -1,6 +1,26 @@
 package com.impulse.back_end.Entity;
 
 public enum UsuarioRole {
-    ROLE_ADMIN,
-    ROLE_USER;
+    ROLE_USER(0),  // Usuario común
+    ROLE_ADMIN(1); // Usuario administrador
+
+    private final int value;
+
+    UsuarioRole(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public static UsuarioRole fromValue(int value) {
+        for (UsuarioRole role : UsuarioRole.values()) {
+            if (role.getValue() == value) {
+                return role;
+            }
+        }
+        throw new IllegalArgumentException("Unexpected value: " + value);
+    }
 }
+
