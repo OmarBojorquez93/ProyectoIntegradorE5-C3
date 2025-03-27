45d766313264b4fdc4c1c3a3d277374faded88aa
Juan Arango <juansantiago@somosmakers.co>
2025-03-27 16:20:26 -0500

    [feat]: Work in progress HUs 29-30-31

--- Backend/src/main/java/com/impulse/back_end/Service/ProductoPublicService.java
+++ Backend/src/main/java/com/impulse/back_end/Service/ProductoPublicService.java
@@ -17,8 +17,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+<<<<<<< HEAD
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+=======
+>>>>>>> 62c2aca ([feat]: Work in progress HUs 29-30-31)
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
