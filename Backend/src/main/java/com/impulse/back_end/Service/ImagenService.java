27f95e6d32577c8edfbb95f08886817e08de03af
Vilma <vilrox.28@gmail.com>
2025-03-31 21:42:48 -0600

    se deshace commit anterior

--- Backend/src/main/java/com/impulse/back_end/Service/ImagenService.java
+++ Backend/src/main/java/com/impulse/back_end/Service/ImagenService.java
@@ -39,7 +39,7 @@ public class ImagenService {
                 return null;
             }
             
-            /*Optional<ProductoEntity> productoOpt = productoRepository.findById(productoId);
+            Optional<ProductoEntity> productoOpt = productoRepository.findById(productoId);
             if (productoOpt.isEmpty()) {
                 logger.error("Producto con ID {} no encontrado", productoId);
                 return null;
@@ -51,7 +51,7 @@ public class ImagenService {
             if (producto.getImagen() != null) {
                 borrarImagen(producto.getImagen().getRuta());
                 imagenRepository.delete(producto.getImagen());
-            }*/
+            }
             
             // Generar un nombre único para el archivo
             String fileName = UUID.randomUUID().toString() + "-" + archivo.getOriginalFilename();
@@ -64,16 +64,16 @@ public class ImagenService {
             
             // Obtener URL pública del archivo
             String fileUrl = s3Client.getUrl(bucketName, fileName).toString();
-            
+            logger.info("FileUrl {}", fileUrl);
             // Guardar la nueva imagen en la BD
             ImagenEntity nuevaImagen = new ImagenEntity(fileUrl);
-            /*nuevaImagen.setProducto(producto);
+            nuevaImagen.setProducto(producto);
             imagenRepository.save(nuevaImagen);
             
             // Asociar la imagen al producto y guardar el producto
             producto.setImagen(nuevaImagen);
             productoRepository.save(producto);
-            */
+
             return nuevaImagen;
         } catch (Exception e) {
             logger.error("Error al subir la imagen para el producto con ID {}", productoId, e);
