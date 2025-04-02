5952f92b63d4ebc3f2da86e742f88d8b5b149239
Matias Torres Contreras <tcontreras993@gmail.com>
2025-04-02 16:39:45 -0600

    Correccion en CardCategory

--- FrontEnd/src/Components/utils/CardCategory.jsx
+++ FrontEnd/src/Components/utils/CardCategory.jsx
@@ -3,8 +3,8 @@ const CardCategory = ({ categoria, filtroPorCategoria }) => {
   const { nombre } = categoria;
 
   const imagen = (nombre) => {
-    if (nombre == "Deportes acuaticos") return "/icono-surf.png";
-    if (nombre == "Camping") return "/icono-camping.png";
+    if (nombre == "Deportes acuaticos") return "icono-surf.png";
+    if (nombre == "Camping") return "icono-camping.png";
     if (nombre == "Deportes invierno") return "invierno.png";
     else return "icono-senderismo.png";
   };
