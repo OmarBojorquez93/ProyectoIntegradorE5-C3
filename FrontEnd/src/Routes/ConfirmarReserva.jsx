ddf0186e302f5822d72fb8ea5253a24aa8af0cc5
Adrianadc-Dev <addiaz802@gmail.com>
2025-04-02 21:49:13 -0500

    ajustes para tablet

--- FrontEnd/src/Routes/ConfirmarReserva.jsx
+++ FrontEnd/src/Routes/ConfirmarReserva.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const ConfirmarReserva = () => {
   return (
     <div className="confirmacionContainer">
-      <h2>Gracias por su referirnos!!!!</h2>
+      <h2>Gracias por preferirnos!</h2>
       <h3>Tu reserva se ha realizado con éxito</h3>
       <Link className="volver" to={"/"}>Seguir alquilando</Link>
     </div>
